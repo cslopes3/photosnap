04fef8fb34a1e9134db2c7288db286e48d3efc0b
cslopes3 <cristianslopes@gmail.com>
2023-12-21 23:29:25 -0300

    add story card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,15 @@
+import { StoryCard } from './components/story-card';
+
 export default function Home() {
-    return <main>Initial Setup</main>;
+    return (
+        <main className="max-w-screen-2xl mx-auto">
+            Initial Setup
+            <section className="flex flex-row flex-wrap">
+                <StoryCard />
+                <StoryCard />
+                <StoryCard />
+                <StoryCard />
+            </section>
+        </main>
+    );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,11 @@ const config: Config = {
                 primary: '#FFFFFF',
                 alt: '#000000',
                 neutral: '#DFDFDF',
-                gradient:
+            },
+            backgroundImage: {
+                'gradient-black':
+                    'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0.27%, rgba(0, 0, 0, 0.66) 100%)',
+                'main-accent':
                     'linear-gradient(27deg, #FFC593 0.42%, #BC7198 43.46%, #5A77FF 83.33%)',
             },
             fontSize: {
@@ -51,10 +55,20 @@ const config: Config = {
                         lineHeight: '1.5625rem',
                     },
                 ],
+                subtitle: [
+                    '0.8125rem',
+                    {
+                        letterSpacing: '0.125rem',
+                    },
+                ],
             },
             textColor: {
                 primary: '#000000',
                 alt: '#FFFFFF',
+            },
+            width: {
+                'mobile-card': '84%',
+                'default-card': '78%',
             },
         },
     },
