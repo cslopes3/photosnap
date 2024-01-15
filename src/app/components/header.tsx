8ba729157472e1c877653a441ec954ce6bf8aa06
cslopes3 <cristianslopes@gmail.com>
2024-01-15 20:39:56 -0300

    fix header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -6,7 +6,7 @@ import { SmallDevicesMenu } from './small-devices-menu';
 export function Header() {
     return (
         <header className="bg-primary z-50 relative">
-            <div className="flex w-5/6 py-7 mx-auto max-w-screen-2xl md:items-center md:w-11/12 md:gap-12 lg:w-3/4 lg:justify-between">
+            <div className="flex w-5/6 py-7 mx-auto max-w-screen-2xl md:justify-between md:items-center md:w-11/12 lg:w-3/4">
                 <div className="flex-grow md:flex-grow-0">
                     <Logo />
                 </div>
@@ -18,7 +18,7 @@ export function Header() {
                 <div className="flex items-center md:hidden">
                     <HamburguerButtonMenu />
                 </div>
-                <button className="py-3 px-6 text-h4 bg-alt text-alt uppercase transition-all hover:text-primary hover:bg-neutral">
+                <button className="hidden py-3 px-6 text-h4 bg-alt text-alt uppercase transition-all md:block hover:text-primary hover:bg-neutral">
                     Get an invite
                 </button>
             </div>
